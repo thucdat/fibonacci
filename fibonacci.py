88a77e4c771d8c5456c849a78195cbad62403a26
- Nguyễn Thúc Đạt - <thucdat@hotmail.com>
2016-11-18 17:40:13 -0800

    Add description

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,4 @@
+# This is the simplest and fastest implementation of the fibonacci sequence in Python.
 def fibonacci(n):
     a = 0
     b = 1
@@ -7,6 +8,12 @@ def fibonacci(n):
         b = temp + b
     return a
 
+# This is the test of the fibonacci sequence
+# This test verifies:
+# The fibonacci is the series of the numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+# The first number is 0
+# The second number is 1
+# The next number is found by adding up the two numbers before it.
 def test_fibonacci(n):
     if n < 2:
         return fibonacci(n) == n
